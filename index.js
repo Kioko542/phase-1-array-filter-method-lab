ad56c4adbc7e766d0cdbe71b458a1ba5f5b963e0
Emmanue <kioxmanu7@gmail.com>
2023-09-29 15:06:01 +0300

    completed asssignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function  matchName(drivers,name){
+    const People = drivers.filter(person => person.name === name);
+    return People
+}
